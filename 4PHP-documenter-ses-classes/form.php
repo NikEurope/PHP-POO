0e86a88c7d9b2fd7567fcf63640844dc7d9125c2
NikEurope <nik.europe@gmail.com>
2016-03-14 15:56:44 +0100

    5

---
+++ 4PHP-documenter-ses-classes/form.php
@@ -0,0 +1,50 @@
+
+<?php
+/**
+*Class Forme
+* Permet de generer un formulaire rapidement
+*/
+
+// classe
+class Form{
+
+  // @var array Donnees utiliser par le formulaire
+  private $data;
+
+  // @var string Teg utilisée pour entourer les champs
+  public $surround = 'p';
+
+  // @var Param : .....
+  // @ return string
+  public function __construct($data = array()){
+     $this->data = $data;
+  }
+
+  // @var Param : HTML : Code html  entourer
+  // @ return string
+ private function surround($html){
+   return "<{$this->surround}>{$html}</{$this->surround}>";
+ }
+
+
+  // @var $index string Index de la valeure à recuperer
+  // @ return string
+private function getValue($index){
+  return isset($this->data[$index]) ? $this->data[$index]: null;
+}
+
+// @param $name string
+// @ return string
+public function input($name){
+    return $this->surround(
+      '<input type="text" name="'.$name.'" value ="'.$this->getValue($name).'">');
+  }
+
+// @ return string
+public function submit(){
+    return $this->surround('<button type="submit">Envoyer</button>');
+  }
+
+}
+
+?>
---
+++ 4PHP-documenter-ses-classes/index.php
@@ -0,0 +1,56 @@
+
+<?php
+// Afficher les erreurs à l'écran
+ini_set('display_errors', 1);
+// Enregistrer les erreurs dans un fichier de log
+ini_set('log_errors', 1);
+// Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+?>
+
+<!-- Form : -->
+<?php
+
+require 'form.php';
+
+$form = new Form(array(
+  'username' => 'Roger'
+));
+
+echo $form->input('username');
+echo $form->input('password');
+echo $form->submit();
+
+
+echo "<br />";
+echo "<br />";
+
+$form = new Form();
+echo $form->input('use');
+echo $form->input('use');
+echo $form->input('use');
+echo $form->input('use');
+echo $form->submit();
+
+echo "<br />";
+echo "<br />";
+
+
+?>
+
+<!-- HTLM + PHP integration de contact forme + remplire autom-->
+
+<?php
+// require 'form.php';
+$form = new Form($_POST);
+?>
+
+<form action="#" method="post">
+
+<?php
+echo $form->input('username');
+echo $form->input('password');
+echo $form->submit();
+?>
+
+</form>
